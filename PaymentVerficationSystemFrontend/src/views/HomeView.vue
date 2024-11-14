af98c35d05d487a6040914b44e7bee4ffa9b20de
Tadiosgb <tadiosgb26@gmail.com>
2024-11-14 22:25:22 +0300

    fropm tadiye

--- PaymentVerficationSystemFrontend/src/views/HomeView.vue
+++ PaymentVerficationSystemFrontend/src/views/HomeView.vue
@@ -65,7 +65,7 @@
             @click.prevent="login()"
             class="mt-5 w-full bg-indigo-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            Sign In(New)
+            Sign In
           </button>
         </form>
       </div>
