dc9ae7c6bb8c9bc93393ccf13138ff8781f9d687
KalayuRedae <46489707+KalayuRedae2016@users.noreply.github.com>
2024-12-30 12:06:54 +0300

    dd

--- PaymentVerficationsystemBackend/Controllers/paymentController.js
+++ PaymentVerficationsystemBackend/Controllers/paymentController.js
@@ -1045,6 +1045,7 @@ exports.getPaymentByMonth = catchAsync(async (req, res, next) => {
     if (formattedPayment[type]) {
       if (formattedPayment[type].paidAt) {
         formattedPayment[type].paidAt = formatDate(formattedPayment[type].paidAt); // Format paidAt date
+        formattedPayment[type].paidAtGC = formatDateGC(formattedPayment[type].paidAt); // Format paidAt date
       }
       if (formattedPayment[type].createdAt) {
         formattedPayment[type].createdAt = formatDate(formattedPayment[type].createdAt); // Format createdAt date
