cbac5cded1c95b4cef041a467471aacce38b29d8
Tadiosgb <tadiosgb26@gmail.com>
2025-01-02 17:21:31 +0300

    ta

--- PaymentVerficationSystemFrontend/src/components/adminaccessiblepages/payment/newPayment/usersCanPay.vue
+++ PaymentVerficationSystemFrontend/src/components/adminaccessiblepages/payment/newPayment/usersCanPay.vue
@@ -5,7 +5,7 @@
       <!-- search and searched users -->
       <div class="">
         <div
-          class="flex items-center justify-between mb-6 bg-white p-4 rounded-lg shadow-md border border-gray-200"
+          class="flex items-center justify-between mb-6 bg-white p-4 rounded-lg shadow-sm border border-gray-200"
         >
           <!-- Search Input -->
           <div class="flex-1 mr-4">
