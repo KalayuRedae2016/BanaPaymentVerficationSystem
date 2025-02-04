5205de312b5a9f08a559f2539e25084ba8d42cb7
KalayuRedae2016 <kalayuredae2@gmail.com>
2025-02-03 20:00:24 -0800

    dd

--- Payment Verfication system Backend/Models/templateModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const TemplateSchema = new mongoose.Schema({
-  name: { type: String, required: true ,unique:true,default:'reciept'},
-  content: { type: String, required: true }, // HTML content as a string
-});
-
-const Template = mongoose.model('Template', TemplateSchema);
-module.exports = Template;
