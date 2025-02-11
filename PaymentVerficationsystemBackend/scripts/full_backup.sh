c578eb6cfd128d320352251080c6993ff8744d32
KalayuRedae2016 <kalayuredae2@gmail.com>
2025-02-11 15:06:43 +0300

    c

--- PaymentVerficationsystemBackend/scripts/full_backup.sh
+++ PaymentVerficationsystemBackend/scripts/full_backup.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-# Define source directories
+# source directories
 SOURCE_DIR="/var/www/BanaPaymentVerficationSystem/"
 MONGODB_DIR="/var/lib/mongodb"
 
-# Define backup directories
+# backup directories
 BACKUP_DIR="/backup/myapp_backup"
 MONGODB_BACKUP_DIR="/backup/mongodb_backup"
 
-# Full paths to commands (modify based on your server environment)
+# Full paths to commands
 RSYNC="/usr/bin/rsync"
 MONGODUMP="/usr/bin/mongodump"
 
-# Load environment variables from .env (ensure correct path to your .env file)
+# Load environment variables from .env
 export $(cat /path/to/your/.env | xargs)
 
 # Ensure backup directories exist
-mkdir -p $BACKUP_DIR
-mkdir -p $MONGODB_BACKUP_DIR
+mkdir -p $BACKUP_DIR #directory for backup application files
+mkdir -p $MONGODB_BACKUP_DIR #directory for backup mongodb files
 
 # Backup MongoDB data using mongodump
 echo "Starting MongoDB backup..."
 $MONGODUMP --uri=$MONGODB_URI --out=$MONGODB_BACKUP_DIR/$(date +"%Y%m%d%H%M%S")
 
-# Backup application files using rsync (comment out if not needed)
-echo "Starting application files backup..."
-$RSYNC -avz --delete $SOURCE_DIR $BACKUP_DIR/$(date +"%Y%m%d%H%M%S")
+# Backup application files using rsync
+# echo "Starting application files backup..."
+# $RSYNC -avz --delete $SOURCE_DIR $BACKUP_DIR/$(date +"%Y%m%d%H%M%S")
 
 # Print success message
 echo "Backup completed successfully!"
