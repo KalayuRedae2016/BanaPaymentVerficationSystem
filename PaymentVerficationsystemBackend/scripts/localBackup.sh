6e4a4411199f90fc710d07e861cd579cf43f0152
KalayuRedae2016 <kalayuredae2@gmail.com>
2025-02-24 10:53:23 +0300

    ff

--- PaymentVerficationsystemBackend/scripts/localBackup.sh
+++ PaymentVerficationsystemBackend/scripts/localBackup.sh
@@ -2,11 +2,10 @@
 
 # Default values (Change if needed)
 BACKUP_SRC="/backup/mongodb_backup"  # MongoDB backup directory on the server -->`ls /backup/mongodb_backup`
-LOCAL_USER="user"                    # Local machine username -->whoami
-LOCAL_IP="192.168.1.100"             # Local machine IP address -->hostname -I or ip
+LOCAL_USER="kaleb"                    # Local machine username -->whoami
+LOCAL_IP="192.168.0.104"             # Local machine IP address -->hostname -I or ip
 LOCAL_PATH="/home/$LOCAL_USER/backup" # Local backup directory -->pwd
 LOG_FILE="/var/log/backup.log"       # Log file for backup process
-
 
 # Construct the remote destination path
 LOCAL_DEST="$LOCAL_USER@$LOCAL_IP:$LOCAL_PATH"
